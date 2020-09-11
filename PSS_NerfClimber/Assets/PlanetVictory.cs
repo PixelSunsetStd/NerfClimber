a29e090c041d19e553ba6c27d86bda19be1e0996
Loganou974 <42536967+Loganou974@users.noreply.github.com>
2020-09-11 17:46:38 +0400

    planet

---
+++ PSS_NerfClimber/Assets/PlanetVictory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetVictory : MonoBehaviour
+{
+    Animator animator;
+    public GameObject planet;
+    BallBehaviour ballBehaviour;
+    GameObject ball;
+
+    bool animLaunched;
+    private void Awake()
+    {
+        ball = GameObject.FindGameObjectWithTag("Player");
+        animator = transform.parent.GetComponent<Animator>();
+        ballBehaviour = ball.GetComponent<BallBehaviour>();
+    }
+
+    
+
+    [ContextMenu("Victory")]
+    public void VictoryAnimation()
+    {
+        planet.transform.position = ball.transform.position;
+        animLaunched = true;
+        animator.SetTrigger("Activate");
+    }
+}
