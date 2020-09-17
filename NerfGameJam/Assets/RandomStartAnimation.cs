b8010da8a651bee494ce705dbfbb837e51b61163
Loganou974 <42536967+Loganou974@users.noreply.github.com>
2020-09-17 13:49:12 +0400

    EndLevel

---
+++ NerfGameJam/Assets/RandomStartAnimation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class RandomStartAnimation : MonoBehaviour
+{
+    [SerializeField]
+    string animationTriggerName = "Activate";
+    Animator animator;
+    public float delay;
+
+    [Space(1)]
+    [SerializeField]
+    bool randomMode = true;
+    [SerializeField]
+    float randomMin = 0;
+    [SerializeField]
+    float randomMax = 1;
+    
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        LaunchAnim();
+
+    }
+
+    private void LaunchAnim()
+    {
+        StartCoroutine(LaunchAnimAfterDelay(delay));
+    }
+
+    void Start()
+    {
+        
+    }
+
+   IEnumerator LaunchAnimAfterDelay(float _delay)
+    {
+        if (randomMode)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(randomMin, randomMax));
+
+        }
+        else
+        yield return new WaitForSeconds(_delay);
+
+        animator.SetTrigger(animationTriggerName);
+    }
+}
---
+++ NerfGameJam/Assets/RandomStartColor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStartColor : MonoBehaviour
+{
+    [SerializeField]
+    Renderer renderer;
+
+    [SerializeField]
+    Material[] randomMats;
+
+
+    private void Awake()
+    {
+        renderer = GetComponentInChildren<Renderer>();
+    }
+    private void Start()
+    {
+        //Getmats
+        Material[] newMats = renderer.materials;
+        int randomNumber = Random.Range(0, randomMats.Length);
+
+
+
+        for (int i = 0; i < renderer.materials.Length; i++)
+        {
+
+            newMats[i] = randomMats[randomNumber];
+        }
+
+        //SetMats
+        renderer.materials = newMats;
+
+    }
+}
