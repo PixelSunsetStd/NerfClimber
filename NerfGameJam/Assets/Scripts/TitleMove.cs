c11daee779f3c83d93bf75c33a66a4636195ffa0
Loganou974 <42536967+Loganou974@users.noreply.github.com>
2020-09-18 08:13:36 +0400

    Props (bonus + targets), Start and Title modif., Tile Move Trigger

---
+++ NerfGameJam/Assets/Scripts/TitleMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Moves away the title when Player enters its trigger
+/// </summary>
+public class TitleMove : MonoBehaviour
+{
+    [SerializeField]
+    string animationTrigger = "MoveAway";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GetComponent<Animator>().SetTrigger(animationTrigger);
+        }
+    }
+}
