afe7c78a9d94eac2a014ce174d89d7cbcd967f33
Loganou974 <42536967+Loganou974@users.noreply.github.com>
2020-09-19 15:55:13 +0400

    BallPool

---
+++ NerfGameJam/Assets/DebugPlayerPrefs.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DebugPlayerPrefs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [ContextMenu("Reset PlayerPrefs")]
+    public void ResetPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
