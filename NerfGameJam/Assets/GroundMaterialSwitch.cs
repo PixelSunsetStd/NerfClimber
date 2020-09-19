d936967182fd3db1b2e04caa24c3f89958927436
Loganou974 <42536967+Loganou974@users.noreply.github.com>
2020-09-19 13:22:20 +0400

    Ground Switch

---
+++ NerfGameJam/Assets/GroundMaterialSwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMaterialSwitch : MonoBehaviour
+{
+    public Material groundMaterial;
+
+    
+    string groundTag = "Ground";
+    private void OnEnable()
+    {
+        Switch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Switch()
+    {
+        GameObject.FindGameObjectWithTag(groundTag).GetComponent<Renderer>().material = groundMaterial;
+    }
+}
